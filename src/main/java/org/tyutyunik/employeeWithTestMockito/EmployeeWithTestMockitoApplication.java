2e00bec1abf55add81ad4dd54539e044ba67bdec
DenTyutyunik <den.tyutyunik@gmail.com>
2024-10-13 15:45:21 +0400

    initial commit

---
+++ src/main/java/org/tyutyunik/employeeWithTestMockito/EmployeeWithTestMockitoApplication.java
@@ -0,0 +1,13 @@
+package org.tyutyunik.employeeWithTestMockito;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeWithTestMockitoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EmployeeWithTestMockitoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/tyutyunik/employeeWithTestMockito/EmployeeWithTestMockitoApplicationTests.java
@@ -0,0 +1,13 @@
+package org.tyutyunik.employeeWithTestMockito;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeWithTestMockitoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
